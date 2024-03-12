3d5862762b32a285afd4539d79f43b979ddbbe80
EffyDoe21 <es.becerra21@ciaf.edu.co>
2024-03-12 17:41:40 -0500

    registro

--- app.py
+++ app.py
@@ -48,6 +48,29 @@ def login():
             # Si las credenciales son incorrectas, volver a la página de inicio de sesión con un mensaje de error
             return render_template('index.html', mensaje="Credenciales incorrectas")
 
+
+#Registro del usuario
+@app.route('/registro')
+def registro():
+    return render_template('registro.html')
+
+@app.route('/crear-registro', methods=["GET","POST"])
+def crear_registro():
+    if request.method == 'POST':
+        correo = request.form.get('txtCorreo')
+        password = request.form.get('txtPassword')
+        city= request.form.get('txtCity')
+        name = request.form.get('txtName')
+        lastname = request.form.get('txtLastname')
+        date = request.form.get('txtDate')
+        
+        cur= mysql.connection.cursor()
+        cur.execute("INSERT INTO usuarios (correo, password, name, lastname, date, city) VALUES (%s, %s, %s, %s, %s, %s)", (correo, password, name, lastname, date, city))
+        mysql.connection.commit()
+        
+        return render_template("index.html", mensaje2="User successfully registered")
+#------------------------------------------------
+
 if __name__ == '__main__':
     app.secret_key = 'Estefania_BQ'
     app.run()
